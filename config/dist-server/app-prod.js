bfe8f793f6b5a862c9589ce1c0b792387d8af3b4
dhl28 <dhl28@126.com>
2017-06-13 22:33:55 +0800

    vue 前端框架基本完成

--- config/dist-server/app-prod.js
+++ config/dist-server/app-prod.js
@@ -16,12 +16,13 @@ app.use('/api/*', apiProxy)
 // app.use('/admin/*',apiProxy)
 // app.use('*.xls',apiProxy)
 // app.use(express.static('assets'))
-app.use('/', express.static(path.join(__dirname, '')))
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
+// app.use('/', express.static(path.join(__dirname, '')))
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.use(function (req, res) {
   // Use res.sendfile, as it streams instead of reading the file into memory.
-  res.sendfile(__dirname + '/index.html')
+  // res.sendfile(__dirname + '/index.html')
+  res.sendfile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(port)
