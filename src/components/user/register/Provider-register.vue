9d2712da9620728fca2b7e9e7d8b25690149de23
dhl28 <dhl28@126.com>
2017-06-28 00:27:37 +0800

    commit files that forgit last commit

---
+++ src/components/user/register/Provider-register.vue
@@ -0,0 +1,23 @@
+//
+Created by douhongliang on 2017/6/27.
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'providerRegister',
+    data () {
+      return {
+        msg: '饭主注册'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
